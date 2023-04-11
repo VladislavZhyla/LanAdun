9ae76a257a67968dab33a7f5687260a86f9f1edd
Vlad zhyla <zhyla.vlad03@gmail.com>
2023-04-11 23:41:50 +0300

    1111

--- LanAdun/Program.cs
+++ LanAdun/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace LanAdun
 {
@@ -97,10 +98,59 @@ namespace LanAdun
         {
             TrainVisualization visualization = new TrainVisualization();
             Console.WriteLine(visualization.CreateTrain(10, "вантажний"));
-            Console.WriteLine(visualization.CreateTrain(5, "пасажирський")); 
+            Console.WriteLine(visualization.CreateTrain(5, "пасажирський"));
             Console.WriteLine(visualization.CreateTrain(2, "ресторанний"));
             Console.WriteLine(visualization.CreateTrain(3, "гальмовиЙ"));
             Console.ReadKey();
         }
     }
+   
+    public class TrainVisualizationTest
+    {
+        public void CreateTrain_Freight_Success()
+        {
+            TrainVisualization visualization = new TrainVisualization();
+            int numberOfCars = 10;
+            string carType = "freight";
+
+            string result = visualization.CreateTrain(numberOfCars, carType);
+
+            string expected = $"Created {numberOfCars} freight cars.";
+            Assert.Equal(expected, result);
+        }
+
+        public void CreateTrain_Passenger_Success()
+        {
+            TrainVisualization visualization = new TrainVisualization();
+            int numberOfCars = 5;
+            string carType = "passenger";
+
+            string result = visualization.CreateTrain(numberOfCars, carType);
+
+            string expected = $"Created {numberOfCars} passenger cars.";
+            Assert.Equal(expected, result);
+        }
+      
+        public void CreateTrain_Dining_Success()
+        {
+
+            TrainVisualization visualization = new TrainVisualization();
+            int numberOfCars = 2;
+            string carType = "dining";
+            string result = visualization.CreateTrain(numberOfCars, carType);
+            string expected = $"Created {numberOfCars} dining cars.";
+            Assert.Equal(expected, result);
+        }
+  
+        public void CreateTrain_Caboose_Failure()
+        {
+            TrainVisualization visualization = new TrainVisualization();
+            int numberOfCars = 3;
+            string carType = "caboose";
+            string result = visualization.CreateTrain(numberOfCars, carType);        
+            string expected = "Could not create the requested type of car.";
+            Assert.Equal(expected, result);
+        }
+    }
+
 }
